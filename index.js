5fde28a8f7e467e79945b88caca553140c81550f
Brigham <brig.johnson@gmail.com>
2022-05-09 05:25:51 -0400

    commit tree 0e53d6ac59b55da5e1b5b7e561b01390e66ad41b

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  //let response = await fetch("https://anapioficeandfire.com/api/books")
+  //let commits = await response.json(); // read response body and parse as JSON
+  //renderBooks(commits);
+
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then( json => {
+      console.log('total num pages is ' + totalNumPages(json));  // 6626
+      return renderBooks(json);
+    })
 }
+
+function totalNumPages(books) {
+  return books.reduce(
+    (acc, curr) => acc = (acc + curr['numberOfPages'])
+  ,0);  // where this is the initial amount
+}
+
+/*
+  1) index.js
+       fetchBooks()
+         renders book titles into the DOM by passing a JSON object to renderBooks():
+     TypeError: Cannot read properties of undefined (reading 'then')
+
+
+    it( "renders book titles into the DOM by passing a JSON object to renderBooks()", async () => {
+      chai.spy.on( window, 'renderBooks' );
+      await fetchBooks().then(() => {
+        expect( window.renderBooks ).to.have.been.called();
+      })
+    } )
+  } )
+*/
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    //h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
